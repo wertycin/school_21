5c90bf7b2b49a50eee158971990417ec50b93912
wertycin <84451964+wertycin@users.noreply.github.com>
2023-08-06 14:25:33 +0300

    Update game_of_life.c

--- day_12/game_of_life.c
+++ day_12/game_of_life.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+// COLORS
+// 40    черный
+// 41    красный
+// 42    зеленый
+// 43    желтый
+// 44    синий
+// 45    пурпурный
+// 46    голубой
+// 47    белый
 #define HEIGHT 25
 #define WIDTH 80
 #define ARCH 100
-#define ALIVE '*'
-#define DEAD '-'
-#define RED "\x1b[31m"
-#define GREEN "\x1b[32m"
+#define ALIVE_I '*'
+#define DEAD_I '-'
+#define ALIVE_O ' '
+#define DEAD_O ' '
+#define BORDER ' '
 #define RESET "\x1b[0m"
+#define ALIVE_C "\x1b[30m"
+#define DEAD_C "\x1b[30m"
+#define BORDER_C "\x1b[30m"
+#define BACKGROUND_A "\x1b[44m"
+#define BACKGROUND_D "\x1b[0m"
+#define BACKGROUND_B "\x1b[47m"
 #define PAUSE 1000000
 #define MAX_FILE_NAME 100
 
@@ -24,13 +39,19 @@ int game_end(int plato[HEIGHT][WIDTH], int archive[ARCH][HEIGHT][WIDTH]);
 int main() {
     char file_name[MAX_FILE_NAME];
     int speed;
-    printf("Choose game speed (1 - min, 5 - max): ");
-    scanf("%d", &speed);
-    speed = PAUSE / speed;
+    printf("Choose game speed (1 - min, 10 - max): ");
+    if (scanf("%d", &speed) != 1 || speed < 1 || speed > 10) {
+        printf("Incorrect speed");
+        exit(0);
+    };
     printf("Select file to play: ");
     scanf("%s", file_name);
     FILE *file;
     file = fopen(file_name, "r");
+    if (file == NULL) {
+        printf("Incorrect filename");
+        exit(0);
+    }
     game_start(file, speed);
     fclose(file);
     return 0;
@@ -53,7 +74,7 @@ void game_start(FILE* file, int speed) {
         print(plato);
         record_plato(plato, archive, &count);
         plato_update(plato);
-        usleep(speed);
+        usleep(PAUSE / speed);
     }
     printf("\e[1;1H\e[2J");
     print(plato);
@@ -63,11 +84,11 @@ void initial_plato(int plato[HEIGHT][WIDTH], FILE* file) {
     char c;
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            if (fscanf(file, "%c", &c) != 1 || (c != ALIVE && c != DEAD)) {
+            if (fscanf(file, "%c", &c) != 1 || (c != ALIVE_I && c != DEAD_I)) {
                 printf("Error!%c is incorrect input!\n", c);
                 exit(0);
             };
-            if (c == ALIVE) {
+            if (c == ALIVE_I) {
                 plato[i][j] = 1;
             } else
                 plato[i][j] = 0;
@@ -82,21 +103,21 @@ void initial_plato(int plato[HEIGHT][WIDTH], FILE* file) {
 
 void print(int plato[HEIGHT][WIDTH]) {
     for (int i = 0; i < WIDTH + 2; i++) {
-        printf("%s%c%s", RED, '.', RESET);
+        printf("%s%s%c%s", BACKGROUND_B, BORDER_C, BORDER, RESET);
     }
     printf("\n");
     for (int i = 0; i < HEIGHT; i++) {
-        printf("%s%c%s", RED, '.', RESET);
+        printf("%s%s%c%s", BACKGROUND_B, BORDER_C, BORDER, RESET);
         for (int j = 0; j < WIDTH; j++) {
             if (plato[i][j])
-                printf("%s%c%s", GREEN, '*', RESET);
+                printf("%s%s%c%s", BACKGROUND_A, ALIVE_C, ALIVE_O, RESET);
             else
-                printf(" ");
+                printf("%s%s%c%s", BACKGROUND_D, DEAD_C, DEAD_O, RESET);
         }
-        printf("%s%s%s", RED, ".\n", RESET);
+        printf("%s%s%c%c%s", BACKGROUND_B, BORDER_C, BORDER, '\n', RESET);
     }
     for (int i = 0; i < WIDTH + 2; i++) {
-        printf("%s%c%s", RED, '.', RESET);
+        printf("%s%s%c%s", BACKGROUND_B, BORDER_C, BORDER, RESET);
     }
     printf("\n");
 }
@@ -157,4 +178,3 @@ int game_end(int plato[HEIGHT][WIDTH], int archive[ARCH][HEIGHT][WIDTH]) {
     }
     return cycling;
 }
-
