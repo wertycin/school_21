a92022cc2211da1755403196f33421428f29953f
wertycin <84451964+wertycin@users.noreply.github.com>
2023-08-10 21:57:59 +0300

    Create door_struct.h

---
+++ day_17/door_struct.h
@@ -0,0 +1,14 @@
+#ifndef DOOR_STRUCT_H
+#define DOOR_STRUCT_H
+
+struct door {
+    int id;
+    int status;
+};
+
+void initialize_doors(struct door* doors);
+void sort(struct door* doors, int n);
+void close(struct door* doors, int n);
+void output(struct door* doors, int n);
+
+#endif
